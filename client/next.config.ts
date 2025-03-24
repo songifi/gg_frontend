355879cb725e813adf6d09fc60fd7129159581d6
Nafisat  Aderayo Adekunle <98043346+Pheenah@users.noreply.github.com>
2025-03-24 16:44:24 +0000

    refac: add custom button

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,37 @@
-import type { NextConfig } from "next";
+/** @type {import('tailwindcss').Config} */
+import designTokens from "@/app/styles/design-tokens"
 
-const nextConfig: NextConfig = {
-  /* config options here */
+
+
+module.exports = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: designTokens.colors.primary,
+        secondary: designTokens.colors.secondary,
+        background: designTokens.colors.background,
+        foreground: designTokens.colors.foreground,
+        border: designTokens.colors.border,
+      },
+      fontFamily: {
+        heading: [designTokens.typography.fontFamily.heading],
+        body: [designTokens.typography.fontFamily.body],
+      },
+      fontSize: {
+        sm: designTokens.typography.fontSize.sm,
+        base: designTokens.typography.fontSize.base,
+        lg: designTokens.typography.fontSize.lg,
+        xl: designTokens.typography.fontSize.xl,
+      },
+      spacing: {
+        xs: designTokens.spacing.xs,
+        sm: designTokens.spacing.sm,
+        md: designTokens.spacing.md,
+        lg: designTokens.spacing.lg,
+        xl: designTokens.spacing.xl,
+      },
+    },
+  },
+  plugins: [],
 };
-
-export default nextConfig;
